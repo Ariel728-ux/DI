bd88e040cde1ad23519ba525ea89bc468a5940cf
Ariel <afeijoov24@fpcoruna.afundacion.org>
2025-11-11 21:05:14 +0100

    Completado Ejercicio 2

--- MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,17 +1,21 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class CatalogActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        Button buttonNavigate = findViewById(R.id.buttonNavigate);
+        buttonNavigate.setOnClickListener(v -> {
+            Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+            startActivity(intent);
+        });
     }
 }
